bab39af590dcff8fea694a9ea8b4d4c9e6ef5948
Neel Parihar <neelparihar599@gmail.com>
2020-04-16 00:09:09 +0530

    added context

--- jobs.py
+++ jobs.py
@@ -3,6 +3,7 @@ from scraper.scraper import amizonebot
 from models.user import UserModel
 from models.attendance import AttendanceModel
 from models.schedule import ScheduleModel
+from app import app
 import hashlib
 
 sched = BlockingScheduler()
@@ -10,25 +11,26 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    users = UserModel.query.all()
-    AttendanceModel.delete()
-    ScheduleModel.delete()
-    for user in users:
-        amizone = amizonebot()
-        amizone.login(user.usern,user.passw)
-        attend = amizone.getAttendance()
-        i=0
-        while i < len(attend):
-            AttendanceModel(
-                user_id=user.id, course_name=attend[i], percentage=attend[i+1], ratio=attend[i+2]).save_to_db()
-            i = i+3
+    with app.app_context():
+        users = UserModel.query.all()
+        AttendanceModel.delete()
+        ScheduleModel.delete()
+        for user in users:
+            amizone = amizonebot()
+            amizone.login(user.usern,user.passw)
+            attend = amizone.getAttendance()
+            i=0
+            while i < len(attend):
+                AttendanceModel(
+                    user_id=user.id, course_name=attend[i], percentage=attend[i+1], ratio=attend[i+2]).save_to_db()
+                i = i+3
 
-        schedule = amizone.getSchedule()
-        i = 1
-        while i < len(schedule):
-            ScheduleModel(
-                user_id=user.id, course_details=schedule[i], prof_name=schedule[i+1]).save_to_db()
-            i = i+2
+            schedule = amizone.getSchedule()
+            i = 1
+            while i < len(schedule):
+                ScheduleModel(
+                    user_id=user.id, course_details=schedule[i], prof_name=schedule[i+1]).save_to_db()
+                i = i+2
     
 
     
