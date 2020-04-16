3c7518bc7fb3fba2a539e4305cfdb81534e6130e
Neel Parihar <neelparihar599@gmail.com>
2020-04-16 16:14:36 +0530

    added next job

--- jobs.py
+++ jobs.py
@@ -9,23 +9,31 @@ import hashlib
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
+@sched.scheduled_job('interval', minutes=3)
+def attendance():
     with app.app_context():
         users = UserModel.query.all()
         AttendanceModel.query.delete()
-        ScheduleModel.query.delete()
         amizone = amizonebot()
         for user in users:
-            
             amizone.login(user.username, user.password)
-            schedule = amizone.getSchedule()
             attend = amizone.getAttendance()
             i = 1
             while i < len(attend):
                 AttendanceModel(
                     user_id=user.id, course_name=attend[i-1], percentage=attend[i+1-1], ratio=attend[i+2-1]).save_to_db()
                 i = i+3
+
+
+@sched.scheduled_job('interval', minutes=4)
+def schedule():
+    with app.app_context():
+        users = UserModel.query.all()
+        ScheduleModel.query.delete()
+        amizone = amizonebot()
+        for user in users:
+            amizone.login(user.username, user.password)
+            schedule = amizone.getSchedule()
             i = 1
             while i < len(schedule):
                 ScheduleModel(
